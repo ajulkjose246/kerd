f21cf57652c55924c4b47248e10855fb39adb314
ajulkjose246 <mail.ajulkjose@gmail.com>
2023-07-10 15:08:26 +0530

    add card option completed

--- lib/screen/home/addCard.dart
+++ lib/screen/home/addCard.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class addCardScreen extends StatefulWidget {
   const addCardScreen({super.key});
@@ -7,9 +11,49 @@ class addCardScreen extends StatefulWidget {
   State<addCardScreen> createState() => _addCardScreenState();
 }
 
+final user = FirebaseAuth.instance.currentUser;
+final CollectionReference cards =
+    FirebaseFirestore.instance.collection("cards");
+TextEditingController cardNumber = TextEditingController();
+TextEditingController cardHolder = TextEditingController();
+TextEditingController cardExp = TextEditingController();
+
 class _addCardScreenState extends State<addCardScreen> {
   @override
   Widget build(BuildContext context) {
+    void addCard() {
+      final data = {
+        'cardExp': cardExp.text,
+        'cardHolder': cardHolder.text,
+        'cardNumber': cardNumber.text,
+        'user': user!.email,
+      };
+      cards.add(data).then((_) {
+        // Insertion successful
+        print('Data inserted successfully');
+        Fluttertoast.showToast(
+            msg: "Card Added successful",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.green,
+            textColor: Colors.white,
+            fontSize: 10.0);
+        Navigator.pushNamed(context, '/auth');
+      }).catchError((error) {
+        // Insertion failed
+        print('Error inserting data: $error');
+        Fluttertoast.showToast(
+            msg: "Card Added Failed",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 10.0);
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
@@ -25,8 +69,11 @@ class _addCardScreenState extends State<addCardScreen> {
           ),
         )),
         Padding(
-          padding: const EdgeInsets.all(15),
+          padding: const EdgeInsets.all(10),
           child: TextField(
+            controller: cardNumber,
+            keyboardType: TextInputType.number,
+            maxLength: 16,
             decoration: const InputDecoration(
               focusedBorder: OutlineInputBorder(
                 borderSide: BorderSide(
@@ -40,8 +87,13 @@ class _addCardScreenState extends State<addCardScreen> {
           ),
         ),
         Padding(
-          padding: const EdgeInsets.all(15),
+          padding: const EdgeInsets.all(10),
           child: TextField(
+            controller: cardHolder,
+            keyboardType: TextInputType.text,
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z]')),
+            ],
             decoration: const InputDecoration(
               focusedBorder: OutlineInputBorder(
                 borderSide: BorderSide(
@@ -55,8 +107,10 @@ class _addCardScreenState extends State<addCardScreen> {
           ),
         ),
         Padding(
-          padding: const EdgeInsets.all(15),
+          padding: const EdgeInsets.all(10),
           child: TextField(
+            controller: cardExp,
+            keyboardType: TextInputType.datetime,
             decoration: const InputDecoration(
               focusedBorder: OutlineInputBorder(
                 borderSide: BorderSide(
@@ -72,7 +126,9 @@ class _addCardScreenState extends State<addCardScreen> {
         Padding(
           padding: const EdgeInsets.all(15.0),
           child: ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              addCard();
+            },
             child: Text("Add Card"),
             style: ButtonStyle(
                 backgroundColor: MaterialStatePropertyAll(Colors.green)),
